f2e4d937dfc3004e743b06e7f2eca2f091d13576
yue <hello@yue.cn>
2025-09-18 00:47:35 +0800

    AI一键导入题目功能搭建

--- YueTiku-admin/src/main/java/com/yuetiku/controller/AiController.java
+++ YueTiku-admin/src/main/java/com/yuetiku/controller/AiController.java
@@ -1,5 +1,12 @@
 package com.yuetiku.controller;
 
+
+import com.alibaba.dashscope.aigc.generation.Generation;
+import com.alibaba.dashscope.aigc.generation.GenerationParam;
+import com.alibaba.dashscope.aigc.generation.GenerationResult;
+import com.alibaba.dashscope.common.Message;
+import com.alibaba.dashscope.common.ResponseFormat;
+import com.alibaba.dashscope.common.Role;
 import com.openai.client.OpenAIClient;
 import com.openai.models.FileCreateParams;
 import com.openai.models.FileObject;
@@ -13,6 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
 
 @RestController
 @Slf4j
@@ -47,11 +55,45 @@ public class AiController {
                 String fileId = fileObject.id();
                 log.info("fileId:{}", fileId);
 
-                return chatClient.prompt()
-                        .system("fileid://"+fileId)
-                        .user("总结一下文档的内容")
+                String content = chatClient.prompt()
+                        .system("fileid://" + fileId)
+                        .user("提取一下文档的的内容")
                         .call()
                         .content();
+
+                log.info("content:{}", content);
+
+                //将文档分析提取加入到结构化输出的条件中
+                Message systemMsg = Message.builder()
+                        .role(Role.SYSTEM.getValue())
+                        .content("""
+                                请你从用户给出的题目内容中提取出题目信息并返回一段严格的 JSON，包含以下字段：
+                                        - type（题目的类型，只能是single,multiple,fill,answer,judge中的一个）
+                                        - title（题目的标题，如果没有就填相应题型的中文,如single对应单选题）
+                                        - content（题目的问题部分,选择题不包括选项）
+                                        - explanation（题目的解析部分，如果没有你就自己根据题目的问题部分给出解析）
+                                        - difficulty（题目的难度,必需是easy,medium,hard中的一个，如果没有就默认medium）
+                                """)
+                        .build();
+
+                Message userMsg = Message.builder()
+                        .role(Role.USER.getValue())
+                        .content(content)
+                        .build();
+
+                Generation gen = new Generation();
+                ResponseFormat jsonMode = ResponseFormat.builder().type("json_object").build();
+                GenerationParam param = GenerationParam.builder()
+                        .apiKey("sk-86efc77040c1423280dc57867fdc1300")
+                        .model("qwen-plus")
+                        .messages(Arrays.asList(systemMsg, userMsg))
+                        .resultFormat(GenerationParam.ResultFormat.MESSAGE)
+                        .responseFormat(jsonMode)
+                        .build();
+                //提取信息
+                GenerationResult call = gen.call(param);
+                String result = call.getOutput().getChoices().get(0).getMessage().getContent();
+                return result;
             }finally {
                 Files.delete(tempFile);
             }
